b6c1da56ad48a89e07c50d59c9db76f9cf170d13
Evan W <sportsdude716@gmail.com>
2018-05-23 23:29:30 -0500

    Organized species data, revised gitignore rules

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pandas as pd
 
 df = pd.read_excel('VAWV-Total.xlsx')
-print(list(df))
 
-print(df)
+# list of unique caves in data frame
+cave_set = sorted(set(df['Cave']))
+
+# list of species found in each cave
+caves = dict()
+
+# rows in data frame associated with each cave
+caves_detailed = dict()
+
+for cave in cave_set:
+    caves[cave] = list(df.loc[df['Cave'] == cave]['Species'])
+    caves_detailed[cave] = df.loc[df['Cave'] == cave]
+
+print(caves)
+print(caves_detailed)
